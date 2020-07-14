7dbeca62aaf88c2047afb48063c2c0ecceddca4d
Bilal <65750280+bilalchrome@users.noreply.github.com>
2020-07-14 15:07:09 +0000

    Add files via upload

---
+++ tb_ep.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script plays whatever episode number of Tigerbelly 
+
+read -p 'Which episode of TigerBelly would you like to play?: ' epvar
+
+mpv $(grep "tb${epvar}" ~/urls.txt | sed "s/tb${epvar}//" | sed "s/^_//")
